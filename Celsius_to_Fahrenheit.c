301332390354d32276a9d361f07ae79b527ae575
AndreeWanya <russianjanek@gmail.com>
2025-02-28 15:46:39 +0700

    Solve the task

--- Celsius_to_Fahrenheit.c
+++ Celsius_to_Fahrenheit.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 float fahr(int cel);
+float celsius(int fahr);
 
 int main() {
     int cel;    // градусы Цельсия
@@ -10,12 +11,23 @@ int main() {
     f = fahr(cel);
     printf("%.2f\n", f);    // .2f - печатать с точностью до 2 знаков после .
 
+    scanf("%d", &cel);
+    f = celsius(cel);
+    printf("%.4f\n", f);
+
     return 0;
 }
 
 float fahr(int cel) {
     float result;
     result = (float)cel * 9 / 5 + 32;
+
+    return result;
+}
+
+float celsius(int fahr) {
+    float result;
+    result = (float)(fahr - 32) * 5 / 9;
 
     return result;
 }
