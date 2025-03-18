b60528f140982faaac2ecb1f0d4a3a16893a489c
AndreeWanya <russianjanek@gmail.com>
2025-03-18 20:36:12 +0700

    Solve the problem Russian peasant's division way

--- Задачи на рекурсию/rusDel.c
+++ Задачи на рекурсию/rusDel.c
@@ -10,24 +10,23 @@ int main() {
 }
 
 int rusDel(int a, int b) {
-    if (a == 0) {
+    if (a == b) {
+        printf("1\n");
         return 1;
     }
-
-    if (b == 1) {
-        printf("%d %d\n", a, b);
-        printf("%d\n", a);
-        return a;
+    int mult, rem;
+    if (a % 2 == 0) {
+        a = a / 2;
+        mult = 2;
+        rem = 0;
+    } else {
+        a = a - b;
+        mult = 1;
+        rem = 1;
     }
-    printf("%d %d\n", a, b);
-    int ost = b % 2;
-    int new_res = 0;
-    if (ost == 1) {
-        new_res = a;
-    }
-    a = a * 2;
-    int res = rusDel(a, b / 2);
-    new_res = new_res + res;
-    printf("%d %d %d %d\n", res, a / 2, ost, new_res);
-    return new_res;
+    printf("%d %d %d\n", a, mult, rem);
+    int part = rusDel(a, b);
+    int new_part = part * mult + rem;
+    printf("%d * %d + %d = %d\n", part, mult, rem, new_part);
+    return new_part;
 }
